fc1340bb1debcc277fc20f626a75d38fb681ba7b
markomilovicEE312018 <milovic.rema@gmail.com>
2021-04-29 20:25:55 +0200

    Funkcija calc

--- Zadatak1/main.c
+++ Zadatak1/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+int Calculation(int a, int b)
+{
+if(a > b)
+return a - b;
+else
+return b - a;
+}
+
+
 int main()
 {
 int a = 6;
 int b = 3;
 int c;
-if(a > b)
-c = a - b;
-else
-c = b - a;
+c=Calculation(a,b);
 printf("Result: %d\n", c);
 return 0;
 }
